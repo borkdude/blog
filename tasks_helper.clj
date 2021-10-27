ecf2dff077d4e811a187e4066b7afa60e80dbb98
Michiel Borkent <michielborkent@gmail.com>
2021-10-27 11:35:42 +0200

    Move code to tasks_helper.clj

--- tasks_helper.clj
+++ tasks_helper.clj
@@ -1,6 +1,8 @@
-(ns tasks-helper)
-
-(require '[clojure.string :as str])
+(ns tasks-helper
+  (:require
+   [babashka.fs :as fs]
+   [clojure.string :as str]
+   [selmer.parser :as selmer]))
 
 (defn parse-opts
   ([opts] (parse-opts opts nil))
@@ -16,3 +18,32 @@
             (assoc opts k v))))
       {:cmds cmds}
       (partition 2 opts)))))
+
+(def opts (parse-opts *command-line-args*))
+
+(def post-template
+  (str/triml "
+{:title {{title | safe }}
+ :file {{file | safe }}
+ :categories {{categories}}
+ :date {{date | safe }}}\n"))
+
+(defn now []
+  (pr-str
+   (.format (java.time.LocalDate/now)
+            (java.time.format.DateTimeFormatter/ofPattern "yyyy-MM-dd"))))
+
+(defn new []
+  (let [{:keys [file title]} opts]
+    (assert file "Must give title")
+    (assert title "Must give filename")
+    (let [post-file (fs/file "posts" file)]
+      (when-not (fs/exists? post-file)
+        (spit (fs/file "posts" file) "TODO: write blog post")
+        (spit (fs/file "posts.edn")
+              (selmer/render post-template
+                             {:title (pr-str title)
+                              :file (pr-str file)
+                              :date (now)
+                              :categories #{:clojure}})
+              :append true)))))
