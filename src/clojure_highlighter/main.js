0b35085f362bf5b1119587daa943e2ef1e60e75b
Michiel Borkent <michielborkent@gmail.com>
2021-11-06 21:08:27 +0100

    runmode wip

--- src/clojure_highlighter/main.js
+++ src/clojure_highlighter/main.js
@@ -1,8 +1,21 @@
-import { tags, styleTags, defaultHighlightStyle } from "@codemirror/highlight";
+import { tags, styleTags } from "@codemirror/highlight";
 import { LezerLanguage } from "@codemirror/language";
 import { EditorState } from "@codemirror/state";
 import { EditorView } from "@codemirror/view";
 import { parser } from "lezer-clojure";
+
+import {highlightTree, defaultHighlightStyle} from "@codemirror/highlight";
+
+function runmode(textContent, language, callback) {
+  const tree = language.parser.parse(textContent);
+  let pos = 0;
+  highlightTree(tree, defaultHighlightStyle.match, (from, to, classes) => {
+    from > pos && callback(textContent.slice(pos, from), null, pos, from);
+    callback(textContent.slice(from, to), classes, from, to);
+    pos = to;
+  });
+  pos != tree.length && callback(textContent.slice(pos, tree.length), null, pos, tree.length);
+}
 
 let theme = EditorView.theme({
   '.cm-content': { 'white-space': 'pre-wrap', padding: '10px 0' },
@@ -42,8 +55,18 @@ let syntax = LezerLanguage.define({parser: cljParser}, {languageData: {commentTo
 let extensions = [EditorView.editable.of(false), theme, defaultHighlightStyle, [syntax]];
 
 document.querySelectorAll("code.clojure").forEach( elt => {
-  new EditorView({state: EditorState.create({doc: elt.innerText.trim(),
-                                             extensions: [extensions]}),
-                  parent: elt});
+  runmode(elt.innerText.trim(), syntax, (content, classes, from, to) => {
+    let span = document.createElement('span');
+    if (classes) {
+      span.classList = classes;
+    }
+    span.innerText = content;
+    elt.appendChild(span);
+    console.log(content, classes, from, to);
+  });
+
+  // new EditorView({state: EditorState.create({doc: elt.innerText.trim(),
+  //                                            extensions: [extensions]}),
+  //                 parent: elt});
   elt.firstChild.remove();
 });
